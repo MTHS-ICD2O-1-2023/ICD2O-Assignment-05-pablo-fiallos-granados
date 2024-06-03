ced81f826700213c8e7bb5844bbb52d38c308415
Pablo Fiallos <159185130+pablo-fiallos-granados@users.noreply.github.com>
2024-06-03 22:01:35 +0000

    everything

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Pablo Fiallos
+// Created on: June 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function enterClicked() {
+  // input
+
+  let iterations = parseInt(document.getElementById("iterations").value)
+  let index = 0
+  let approximation = 0.5
+
+  // process
+
+  while (index < iterations) {
+    approximation = 1.0 / (2.0 + approximation)
+    index += 1
+  }
+  approximation += 1
+
+  // output
+
+  document.getElementById("user-info").innerHTML = approximation
+}
